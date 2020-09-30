bc73ab9e75fff0f2a292355be18b0a67eaad6ae7
Embersands game Studios <eitan.levy.159@gmail.com>
2020-09-30 17:35:34 +0300

    CU-4hrjmx[in progress] Add enemy health

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    #region Variables
+
+    public int maxHealth;
+    private int currentHealth;
+
+    #endregion
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Changes enemy's health
+    /// </summary>
+    /// <param name="amount"></param>
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+    }
+}
